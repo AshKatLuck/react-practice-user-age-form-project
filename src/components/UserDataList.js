041fc00b645e64a2f42fadce832ff192df1e5fa0
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-01-16 21:58:33 -0800

    add styles to the userDataList

--- src/components/UserDataList.js
+++ src/components/UserDataList.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Card from "../UI/Card";
+import classes from "./UserDataList.module.css";
 const UserDataList = (props) => {
   return (
-    <div>
-      {props.userData.map((user) => {
-        const content = user.name + "(" + user.age + " years old)";
-        return <Card>{content}</Card>;
-      })}
-    </div>
+    <Card className={classes.users}>
+      <ul>
+        {props.userData.map((user) => {
+          const content = user.name + "(" + user.age + " years old)";
+          return <li>{content}</li>;
+        })}
+      </ul>
+    </Card>
   );
 };
 export default UserDataList;
